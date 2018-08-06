0136a5c36fb99b0e464d8e00a1c432cca9ee7f52
David Kramer <daveskypilot@gmail.com>
2018-08-05 21:04:50 -0600

    Add new action types for pages

--- src/actions/types.js
+++ src/actions/types.js
@@ -2,3 +2,8 @@
 export const PAGE_META_REQUEST = 'PAGE_META_REQUEST';
 export const PAGE_META_SUCCESS = 'PAGE_META_SUCCESS';
 export const PAGE_META_FAILURE = 'PAGE_META_FAILURE';
+
+// page actions
+export const PAGE_REQUEST = 'PAGE_REQUEST';
+export const PAGE_SUCCESS = 'PAGE_SUCCESS';
+export const PAGE_FAILURE = 'PAGE_FAILURE';
